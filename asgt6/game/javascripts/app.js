98e38e73ed2625c3cb685b9acfd81b1d2c18ff79
brunacruz11 <cruzbruna@gmail.com>
2015-04-28 00:09:02 -0700

    adding the index and javascript files

---
+++ asgt6/game/javascripts/app.js
@@ -0,0 +1,41 @@
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
+
+var main = function () { 
+    "use strict";
+    
+    var $text;
+    var $message;
+    var $table;
+    var $td;
+    var $tr;
+    var play = {};
+    
+    $("#formId button").on("click", function (event) {
+        
+        $text = $("#myselect option:selected").text();
+        play.move = $text;
+        $("#myselect").val("rock");
+
+        $.ajax({
+            type: "POST",
+            url: "/play/"+play.move,
+            data: play,
+            dataType: 'json',
+            success: function (data) {
+        
+                $("main .output p").text("Scores:");
+                $("#wins td:first-child").text("Wins:");
+                $("#wins td:nth-child(2)").text(data.wins);
+                $("#losses td:first-child").text("Losses:");
+                $("#losses td:nth-child(2)").text(data.losses);
+                $("#ties td:first-child").text("Ties:");
+                $("#ties td:nth-child(2)").text(data.ties);
+                $("#lastOutcome td:first-child").text("Last outcome:");
+                $("#lastOutcome td:nth-child(2)").text(data.lastOutcome);
+            
+            }
+        });       
+    });
+};
+
+$(document).ready(main);
